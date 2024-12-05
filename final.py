a260f93dc14c2b834b505dcf79c4a40aa44568d8
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-05 11:47:00 -0500

    update

--- final.py
+++ final.py
@@ -8,27 +8,25 @@ import csv
 import unittest
 import requests
 
-def load_menu_items(html_file): 
+def load_menu_items(soup): 
     """
     INPUT: A string containing the path of the html file
     RETURN: A list of tuples
     """
 
-    with open(html_file, "r", encoding="utf-8-sig") as file:
-            content = file.read()
-            soup = BeautifulSoup(content, "html.parser")
+ 
+    product_title = []
+    
+    food_containers = soup.find_all("div", class_="c-product-card__info__container")
+    
+    for food in food_containers:
+        title = food.find("span", class_="c-product-card__name")
+            
+        if title:
+            product_title.append(title.text.strip())
+        else:
+            product_title.append("No Title Available")
 
-            product_title = []
-            
-            food_containers = soup.find_all("div", class_="c-product-card__info__container")
-            
-            for food in food_containers:
-                title = food.find("span", class_="c-product-card__name")
-                    
-                if title:
-                    product_title.append(title.text.strip())
-                else:
-                    product_title.append("No Title Available")
     print(product_title)
     return product_title
 
@@ -51,7 +49,7 @@ class TestAllFunctions(unittest.TestCase):
         self.menu_items = load_menu_items(soup)
 
     def test_load_menu_items(self):
-
+        
         self.assertEqual(type(self.menu_items), list)
 
 
